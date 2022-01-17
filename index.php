c34648160e2bec870f75a40268ea1cdd41257a6f
mathilderenversade <mathirenversade@gmail.com>
2022-01-17 12:00:37 +0100

    display views

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 // Point d'entrée pour la page d'accueil
 
 // Inclusion des fichiers nécessaires
+//var_dump(__FILE__);
+
+require __DIR__.'/inc/templates/header.tpl.php';
+require __DIR__.'/inc/templates/home.tpl.php';
+require __DIR__.'/inc/templates/footer.tpl.php';
+
 
 // Récupération des données nécessaires la page (si besoin)
 
