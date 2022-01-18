66f110fa5b79f0c5d7eaeb6e0a61bb945deeb8e3
mathilderenversade <mathirenversade@gmail.com>
2022-01-18 10:10:13 +0100

    page author and category done

--- category.php
+++ category.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__.'/inc/data/datas.php';
+
 require __DIR__.'/inc/templates/header.tpl.php';
 require __DIR__.'/inc/templates/category.tpl.php';
 require __DIR__.'/inc/templates/footer.tpl.php';
